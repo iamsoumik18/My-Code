8957c56f1260303f4552d2e8646ed5eff6234d8e
Soumik Saha <iamsoumik18@gmail.com>
2021-04-09 18:03:41 +0530

    Files Updated

--- codeChef/BinaryStringMEX.java
+++ codeChef/BinaryStringMEX.java
@@ -12,23 +12,87 @@ public class BinaryStringMEX {
 			int t = Integer.parseInt(br.readLine());
 			while(t-->0) {
 				String s = br.readLine();
-				boolean flag = false;
-				for(int i=0; i<s.length(); i++) {
-					if(s.charAt(i)=='0')
-						flag = true;
-				}
-				if(!flag) {
-					bw.write("0\n");
-					bw.flush();
-				}else {
-					bw.write(sol(s)+"\n");
-					bw.flush();
-				}
+				bw.write(sol2(s)+"\n");
+				bw.flush();
 			}
 		}catch(Exception e) {
 			return;
 		}
 
+	}
+	
+	static long count(String s1, char ch) {
+		int cnt = 0;
+		for(int i=0; i<s1.length(); i++)
+			if(s1.charAt(i)==ch)
+				cnt++;
+		return cnt;
+	}
+	
+	static String sol2(String s) {
+		
+		if(count(s,'0') == 0){ 
+            return "0";
+        }
+		
+        if(count(s,'1') == 0){ 
+            return "1";
+        }
+
+        int n = s.length(), c = 0;
+        char bs[][]  = new char[n+1][2];
+        bs[n][0] = '0';
+        bs[n][1] = '0';
+
+        for(int i = n-1; i >= 0; i--){
+            bs[i][0] = bs[i+1][0];
+            bs[i][1] = bs[i+1][1];
+            if(s.charAt(i) == '0'){
+                bs[i][1] = '1';
+            }
+            else{
+                bs[i][0] = '1';
+            }
+            if(bs[i][0] == '1' && bs[i][1] == '1') {
+                c++; 
+                bs[i][0] = '0';
+                bs[i][1] = '0';
+            }
+        }
+        StringBuffer sb = new StringBuffer("");
+        boolean flag = true;
+        int cur = 0;
+        if(bs[0][1] == '0' && bs[0][0] == '1'){
+            sb.append("1");
+            flag = false;
+        }
+        c++;
+
+        while(c-->0){
+            if(flag){
+                if(bs[0][0] == '0'){
+                    sb.append("1");
+                    while(cur < n && s.charAt(cur) != '1')
+                        cur++;
+                    cur++;
+                }
+                flag = false;
+            }
+            else{
+            	int temp[] = {1,0};
+                for(int i : temp){
+                    if(bs[cur][i] == '0'){
+                        char ch = (char)('0' + 1 - i);
+                        sb.append(ch);
+                        while(cur < n && s.charAt(cur) != ch)
+                            cur++;
+                        cur++;
+                        break;
+                    }
+                }
+            }
+        }
+        return sb.toString();
 	}
 	
 	static String sol(String s) {
